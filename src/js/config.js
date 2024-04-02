0c0817fe7cf1510611f40b4e11fd20873fdbaffb
ChhotwantSaw <8434cks@gmail.com>
2024-04-02 23:37:37 +0530

    Initial comit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '43a1651b-e92f-42c6-8ec6-c9312dafba21';
+export const MODAL_CLOSE_SEC = 2.5;
